e79955412ea555db8e42dac9d871fc6ab78e135d
Anton Veldanov <anton.veldanov@gmail.com>
2019-07-04 17:02:32 -0700

    Hooks

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -13,7 +13,7 @@ const ContactItem = ({ contact }) => {
     <div className="card bg-light">
       <h3 className="text-primary text-left">
         {name}{' '} <span
-          style={{ float: 'right' }}
+          style={{ float: "right" }}
           className={'badge ' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
       </h3>
       <ul>
